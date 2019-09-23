b3e7bf66869a0ac3b45a5da0c83e5403e748211f
cheche062 <yoojunglee@126.com>
2019-09-23 17:09:37 +0800

    我的 完善

---
+++ shopping-street/pages/profile/w-list-view/w-list-view.js
@@ -0,0 +1,26 @@
+// pages/profile/w-list-view/w-list-view.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
